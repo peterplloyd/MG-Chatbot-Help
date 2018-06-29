7913ea2209fe0086bb3e0067ec27ab33a982dff4
Peter Lloyd <peter.lloyd@missguided.com>
2018-06-29 14:59:31 +0100

    Output Data to a txt file

--- bot.js
+++ bot.js
@@ -14,112 +14,125 @@ var Twitter = new TwitterPackage(secret);
 var comprehend = new AWS.Comprehend({ apiVersion: "2017-11-27" });
 
 // Call the stream function and pass in 'statuses/filter', our filter object, and our callback
-//Twitter.stream("statuses/filter", { track: "#mghackhelp" }, function(stream) {
-// ... when we get tweet data...
+Twitter.stream("statuses/filter", { track: "#mghackhelp" }, function(stream) {
+  // ... when we get tweet data...
+  stream.on("data", function(tweet) {
+    var keywords = ["order", "price", "wrong", "return"];
 
-// stream.on("data", function(tweet) {
-var keywords = ["order", "price", "wrong", "return"];
-stream(keywords);
+    var params = {
+      LanguageCode: "en",
+      Text: tweet.text
+    };
 
-function stream(keywords) {
-  // print out the text of the tweet that came in
-
-  //This is to test with fake tweets without having to post them
-  var tweet = { text: "Angry about rubbish money price" };
-
-  var params = {
-    LanguageCode: "en",
-    Text: tweet.text
-  };
-
-  function detectKeyPhrases(params, cb) {
-
-    comprehend.detectKeyPhrases(params, function(err, data) {
-      if (err) {
-        console.log(err, err.stack);
-      } else {
-        cb(data);
-      }
-    });
-  }
-
-  //Get Sentiment Data
-  var sentimentData = detectSentiment(params);
-
-  function detectSentiment(params, cb) {
-    comprehend.detectSentiment(params, function(err, data) {
-      if (err) {
-        console.log(err, err.stack);
-      } else {
-        cb(data);
-      }
-    });
-  }
-
-  //Check message for keywords
-  var registeredKeyword = findWord(keywords, tweet.text);
-
-  function findWord(keywords, tweet) {
-    var match;
-
-    keywords.forEach(function(el) {
-      if (RegExp("\\b" + el + "\\b").test(tweet)) {
-        match = el;
-      }
-    });
-    return match;
-  }
-
-  //Response
-  var customerResponse = getResponse(registeredKeyword);
-
-  function getResponse(registeredKeyword) {
-    //Order
-    switch (registeredKeyword) {
-      case "order":
-        return "Dang girl! Lets go track that order hun!";
-      //Wrong Price
-      case "price":
-        return "I wish everything was free! Slide into our DMs and we'll sort it out";
-      //Wrong Size
-      case "size":
-        return "Gotta look fly in the right swag! Lets get your stuff sorted. missguided.co.uk/help";
-      //Returns
-      case "return":
-        return "Sorry you didn't like it! Gotta look fire for the 'gram";
-      default:
-        return "Please send us a DM and we'll get back to you asap";
+    function detectKeyPhrases(params, cb) {
+      comprehend.detectKeyPhrases(params, function(err, data) {
+        if (err) {
+          console.log(err, err.stack);
+        } else {
+          cb(data);
+        }
+      });
     }
-  }
 
-  //build our reply object
-  /* var statusObj = {
+    //Get Sentiment Data
+    function detectSentiment(params, cb) {
+      comprehend.detectSentiment(params, function(err, data) {
+        if (err) {
+          console.log(err, err.stack);
+        } else {
+          cb(data);
+        }
+      });
+    }
+
+    //Check message for keywords
+    var registeredKeyword = findWord(keywords, tweet.text);
+
+    function findWord(keywords, tweet) {
+      var match;
+
+      keywords.forEach(function(el) {
+        if (RegExp("\\b" + el + "\\b").test(tweet)) {
+          match = el;
+        }
+      });
+      return match;
+    }
+
+    //Response
+    var customerResponse = getResponse(registeredKeyword);
+
+    function getResponse(registeredKeyword) {
+      //Order
+      switch (registeredKeyword) {
+        case "order":
+          return "Dang girl! Lets go track that order hun!";
+        //Wrong Price
+        case "price":
+          return "I wish everything was free! Slide into our DMs and we'll sort it out";
+        //Wrong Size
+        case "size":
+          return "Gotta look fly in the right swag! Lets get your stuff sorted. missguided.co.uk/help";
+        //Returns
+        case "return":
+          return "Sorry you didn't like it! Gotta look fire for the 'gram";
+        default:
+          return "Please send us a DM and we'll get back to you asap";
+      }
+    }
+
+    //build our reply object
+    var statusObj = {
       status: "Hi @" + tweet.user.screen_name + ",",
       in_reply_to_status_id: "" + tweet.id_str
-    };*/
+    };
 
-  detectKeyPhrases(params, data => {
-    var analyticsResults =
-      "Tweet: " +
-      tweet.text +
-      "\nKeyWords: " +
-      registeredKeyword +
-      "\nReply: " +
-      customerResponse +
-      "\nSentiment: " +
-      "\nPhrase Data: " +
-      JSON.stringify(data);
+    detectKeyPhrases(params, data => {
+      var analyticsResults =
+        "Tweet: " +
+        tweet.text +
+        "\nKeyWords: " +
+        registeredKeyword +
+        "\nReply: " +
+        customerResponse +
+        "\nPhrase Data: " +
+        JSON.stringify(data);
 
-    fs.writeFile("results.txt", analyticsResults, "utf8", function(err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("The file was saved!");
+      fs.appendFileSync(
+        "results.txt",
+        analyticsResults,
+        "utf8",
+        { flags: "a+" },
+        function(err) {
+          if (err) {
+            return console.log(err);
+          } else {
+            console.log("The file was saved!");
+          }
+        }
+      );
     });
-  });
 
-  //call the post function to tweet something
-  /* Twitter.post("statuses/update", statusObj, function(
+    detectSentiment(params, data => {
+      var analyticsResults = "\nSentiment: " + JSON.stringify(data);
+
+      fs.appendFileSync(
+        "results.txt",
+        analyticsResults,
+        "utf8",
+        { flags: "a+" },
+        function(err) {
+          if (err) {
+            return console.log(err);
+          } else {
+            console.log("The file was saved!");
+          }
+        }
+      );
+    });
+
+    //call the post function to tweet something
+    Twitter.post("statuses/update", statusObj, function(
       error,
       tweetReply,
       response
@@ -128,11 +141,11 @@ function stream(keywords) {
       if (error) {
         console.log(error);
       }
-    });*/
-}
-
-//... when we get an error...
-/*stream.on("error", function(error) {
+    });
+  });
+  //... when we get an error...
+  stream.on("error", function(error) {
     //print out the error
     console.log(error);
-  });*/
+  });
+});
